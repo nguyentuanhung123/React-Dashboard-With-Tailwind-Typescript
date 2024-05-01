c543c6770c443f45594e7efc1358c22bd9b88703
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2024-05-01 17:42:12 +0700

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import StatCards from "./components/StatCards"
+import Layout from "./layout"
+
+const App = () => {
+  return (
+    <Layout>
+      <StatCards />
+    </Layout>
+  )
+}
+
+export default App
